cc634f42c241e2abec583c328dd6a91238f8a987
kakedashie1 <126971420+kakedashie1@users.noreply.github.com>
2026-05-24 16:06:14 +0900

    [update] dockerfileデプロイ失敗修正app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,13 +6,25 @@ import { createRoot } from "react-dom/client";
 
 createInertiaApp({
     title: (title) => `${title} - 学習時間記録アプリ`,
+
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        return pages[`./Pages/${name}.jsx`].default;
+
+        const page = pages[`./Pages/${name}.jsx`];
+
+        if (!page) {
+            console.error("Inertia page not found:", name);
+            console.error("Available pages:", Object.keys(pages));
+            throw new Error(`Inertia page not found: ${name}`);
+        }
+
+        return page.default;
     },
+
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
+
     progress: {
         color: "#4B5563",
     },
