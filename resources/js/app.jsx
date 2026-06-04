dbef2ef7e6ce18fec7c8e4d264f7fd5fe7e82d99
kakedashie1 <126971420+kakedashie1@users.noreply.github.com>
2026-06-04 22:25:52 +0900

    [update] PWA化

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -31,3 +31,23 @@ createInertiaApp({
         color: "#4B5563",
     },
 });
+
+// ↓追加
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then((registration) => {
+                console.log(
+                    "Service Worker registered:",
+                    registration
+                );
+            })
+            .catch((error) => {
+                console.error(
+                    "Service Worker registration failed:",
+                    error
+                );
+            });
+    });
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>Study Record</title>
+    <link rel="manifest" href="/manifest.webmanifest">
+    <meta name="theme-color" content="#2563eb">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
